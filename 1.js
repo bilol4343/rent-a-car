ef6555a038f89a9170c940d9ede50bdb1b665bc6
bilol4343 <wwpubg113@gmail.com>
2025-11-11 08:50:17 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+const modeToggle = document.getElementById('mode-toggle');
+
+modeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  modeToggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+});
